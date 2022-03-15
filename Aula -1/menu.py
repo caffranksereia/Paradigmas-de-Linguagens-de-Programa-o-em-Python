2242e74a8b0cd6bfc776e9f3db3ed8016a44f1a5
ferafael <circuncisaofabio@icloud.com>
2022-03-15 14:51:56 -0300

    brincando um pouco

---
+++ Aula -1/menu.py
@@ -0,0 +1,13 @@
+a=[]
+i = 0
+menu = True
+while menu:
+    print(
+    """1. criar_lista
+    2.inserir
+    3.ver a lista
+    4. excluir um item """
+   )
+    
+    opc = int(input("Digite uma opção:"))
+    print(opc)
