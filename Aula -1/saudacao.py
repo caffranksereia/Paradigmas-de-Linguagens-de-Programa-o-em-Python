b07af14a647d7d42e64a73e90f2c3bedc3e4866a
Fábio Eduardo Circuncisão <32108788+caffranksereia@users.noreply.github.com>
2022-07-06 20:41:28 -0300

    Add files via upload

---
+++ Aula -1/saudacao.py
@@ -0,0 +1,9 @@
+def saudacao(name):
+    if name == None:
+        print('hello sweet')
+    else:
+        print(f'hello sweet {name}')
+        
+
+x = input('qual é o seu nome:')        
+saudacao(x)
