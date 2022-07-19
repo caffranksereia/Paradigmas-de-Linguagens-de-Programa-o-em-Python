4eff4213fb97a4d8e31a13c716f43f38a5d3a669
Fábio Eduardo Circuncisão <32108788+caffranksereia@users.noreply.github.com>
2022-07-18 22:09:20 -0300

    Add files via upload

---
+++ Aula -1/exerciciosomaduaslistas.py
@@ -0,0 +1,14 @@
+A = [1,2,3,4,5,6,7]
+B = [1,2,3,4]
+lista_result = []
+# lista_result = [(a+b) for a,b in zip(A,B)]
+# print(lista_result)
+
+# for i in range(len(B)):
+#     lista_result.append(A[i] + B[i])
+# print(lista_result)
+
+
+# for i,_ in  enumerate(B):
+#     lista_result.append(A[i]+B[i])
+# print(lista_result)
